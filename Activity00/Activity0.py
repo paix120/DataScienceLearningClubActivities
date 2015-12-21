5aa520a1114141eaaf88d0ad7f11a6854808ae19
paix120 <renee.parilak@gmail.com>
2015-12-20 20:48:53 -0500

    add activity 0 hello world

--- Activity00/Activity0.py
+++ Activity00/Activity0.py
@@ -1,10 +1,12 @@
 #import pandas and numpy libraries
 import pandas as pd
 import numpy as np
-import sys
+import sys #sys needed only for python version
 
+#display versions of python and packages
 print('\npython version ' + sys.version)
 print('pandas version ' + pd.__version__)
 print('numpy version ' + np.__version__)
 
+#say hello!
 print('\nHello World!')
