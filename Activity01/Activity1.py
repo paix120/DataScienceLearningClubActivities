fb0d1689c8263b3a76a4b5ba2b0813d9fb69c34d
paix120 <renee.parilak@gmail.com>
2015-12-22 01:03:01 -0500

    fix some comments and make more readable
    better labeling what I'm doing in each section

--- Activity01/Activity1.py
+++ Activity01/Activity1.py
@@ -4,10 +4,14 @@ import numpy as np
 
 # read in text file, which is tab delimited, and set global unique indentifier as the index column
 df = pd.read_csv('sample_ebird_data.txt', sep='\t', index_col='GLOBAL UNIQUE IDENTIFIER')
-# display the first 5 rows to view data
-print(df.head())
+
+#force it to display wider in the console instead of wrapping so narrowly
+pd.set_option('display.width', 250)
+# display the first 5 rows to view example data
+print(df.head(n=10))
+
 # display summary stats (at least for numeric columns)
-print(df.describe())
+print(df.describe)
 # check to make sure unique identifier is unique
 print('\nIndex unique? ' + str(df.index.is_unique))
 
@@ -18,10 +22,10 @@ cn = cn[cn > 2]
 # display the list of species seen 3 or more times
 print('\nSpecies seen in more than 2 sightings, by common name:')
 print(cn)
+
 # calculate the total of these birds counted and display
 # print('\n Count of birds sighed by common name:')
 # print(df.pivot_table('OBSERVATION COUNT',index='COMMON NAME', aggfunc='sum'))
-
 # what are those Xs messing up the sums?
 # (it's the 2nd most common value)
 # print(df['OBSERVATION COUNT'].value_counts())
@@ -31,22 +35,22 @@ df_counting = pd.DataFrame(df['OBSERVATION COUNT'].replace(to_replace='X', value
 # rename the column so it will have a different name than original column after merge
 df_counting.rename(columns={'OBSERVATION COUNT': 'OBS COUNT MODIFIED'}, inplace=True)
 df = pd.concat([df, df_counting], axis=1)
-# print(df.head)
-# try again...
-# calculate the total of these birds counted and display
-#print('\n Count of birds sighted by common name using pivot table:')
+
+# calculate the total of birds counted, using a pivottable
 df_pivot = pd.DataFrame(df.pivot_table('OBS COUNT MODIFIED', index='COMMON NAME', aggfunc='sum'))
 df_pivot.rename(columns={'OBS COUNT MODIFIED': 'PIVOTED COUNT'}, inplace=True)
 #print(df_pivot)
 # compare pivot to groupby
-#print('\n Count of birds sighted by common name using groupby:')
 df_grouped = pd.DataFrame(df.groupby(['COMMON NAME'])['OBS COUNT MODIFIED'].sum())
 df_grouped.rename(columns={'OBS COUNT MODIFIED': 'GROUPED COUNT'}, inplace=True)
 #print(df_grouped)
-print('\nCount of birds sighted by common name:')
-pivot_group_compare = pd.concat([df_pivot, df_grouped], axis=1)
+#combine so you can see side by side and display the list
+#print('\nCount of birds sighted by common name:')
+#pivot_group_compare = pd.concat([df_pivot, df_grouped], axis=1)
 #print(pivot_group_compare)
+
 #print common names where more than 2000 birds were sighted
+print('\nCount of species with over 2000 birds sighted:')
 print(df_grouped[df_grouped['GROUPED COUNT'].astype('int') > 2000])
 
 #i want to know how many sightings are represented
@@ -59,9 +63,11 @@ df_counting = pd.DataFrame(df.pivot_table('OBS COUNT MODIFIED', index='COMMON NA
 df_counting.rename(columns={'OBS COUNT MODIFIED': 'OBSERVATIONS'}, inplace=True)
 df_grouped.rename(columns={'GROUPED COUNT': 'BIRDS COUNTED'}, inplace=True)
 
-#get min latitude
+#determine min latitude for any bird sighting in dataset
 print('\nOverall Min Latitude: ' + str(df.min()['LATITUDE']) )
-#get min latitude per species
+
+#get min latitude per species and combine the dataframes
+#and rename the columns before recombining so we can tell what summarization is in each
 df_min_lats =  pd.DataFrame(df.pivot_table('LATITUDE', index='COMMON NAME', aggfunc='min'))
 df_min_lats.rename(columns={'LATITUDE': 'MIN LATITUDE'}, inplace=True)
 df_max_lats =  pd.DataFrame(df.pivot_table('LATITUDE', index='COMMON NAME', aggfunc='max'))
